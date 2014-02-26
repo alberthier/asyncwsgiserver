a3ee381d1b178a93546ef99dd8d70694195a7ca1
Éric ALBER <eric.alber@gmail.com>
2014-02-26 23:51:34 +0100

    Add example

---
+++ example/hello.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import asyncore
+import asyncwsgiserver
+
+
+def hello_app(environ, start_response):
+    """ Simple WSGI application """
+    status = '200 OK'
+    response_headers = [('Content-type', 'text/plain')]
+    start_response(status, response_headers)
+    return [b'Hello world!\n']
+
+
+if __name__ == "__main__":
+    # Create the web server on port 8000
+    port = 8000
+    print("Open http://localhost:" + str(port))
+    asyncwsgiserver.WsgiServer("", 8000, hello_app)
+    try:
+        # Start asyncore's event loop
+        asyncore.loop()
+    except KeyboardInterrupt:
+        pass
